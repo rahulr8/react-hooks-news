1003cbb6f8c881b506a6d71a72a5fe3413d772b3
Rahulio <rahuliodev8@gmail.com>
2020-03-26 10:17:44 -0400

    Fetch data with useEffect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function App() {
-  return <div>App</div>;
+  const [results, setResults] = useState([]);
+
+  // UseEffect should return a cleanup function or nothing
+  // It cannot return a promise
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const response = await axios.get(
+      "http://hn.algolia.com/api/v1/search?query=reacthooks"
+    );
+    setResults(response.data.hits);
+  };
+
+  return (
+    <>
+      <ul>
+        {results.map(result => (
+          <li key={result.objectID}>
+            <a href={result.url}>{result.title}</a>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
